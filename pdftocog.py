d9511b61bcffa0152d25064bbc81020f5a6f032d
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-16 10:43:41 -0500

    generated 6,30,100

--- pdftocog.py
+++ pdftocog.py
@@ -1,22 +1,26 @@
 import numpy as np
 from glob import glob
 import string
-from sklearn.metrics import mean_squared_error as mse
+import os
 from textblob import TextBlob
 import unicodedata
 import codecs
+import re
 
 def make_window_weights(window):
     weights = np.concatenate((np.linspace(0,window,num=window),[0],np.linspace(window,0,num=window)))
     return weights
+
+def only_capitals(s):
+    return re.sub(r'[^A-Z]', '', s)
 
 def readtolist(f):
     text = []
     with codecs.open(f, mode='r', encoding='utf-8') as F:
         for line in F.readlines():
             line = unicodedata.normalize('NFKD',line).encode('ascii','ignore')
-            text.extend([X.translate(None,string.punctuation+string.digits+string.whitespace).upper() for X in line.split(" ")])
-        text = filter(lambda x: len(x.strip())>1, text)
+            text.extend([only_capitals(X.translate(None,string.punctuation+string.digits+string.whitespace).upper()) for X in line.split(" ")])
+        text = filter(lambda x: len(x.strip())>2, text)
     return text
 
 def find_all(tblob,word):
@@ -44,6 +48,7 @@ def textblobber(pdf,uniques,wordmatrix,window,w):
     text = readtolist(pdf)
     tblob = TextBlob(" ".join(text))
     for j,word in enumerate(uniques):
+        print str(j)+"of"+str(len(uniques))+"words:"+word
         indices = find_all(tblob,word)
         for i in indices:
             sl_start = max(0, i-window)
@@ -63,11 +68,19 @@ file4 = '/home/rschadmin/Data/scraped/21273403.txt'
 file5 = '/home/rschadmin/Data/scraped/21277171.txt'
 file6 = '/home/rschadmin/Data/scraped/19580873.txt'
 files = [file1,file2,file3,file4,file5,file6]
-files = glob('/home/rschadmin/Data/scraped/*.txt')
-uniques= unique_words([file6])
-#window = 4
-#w = np.array([1,2,3,4,0,4,3,2,1]) #window weight
+files = glob('/home/rschadmin/Data/scraped/*.txt')[:100]
+uniques_file = "/home/rschadmin/Soft/pdftocog/uniques_100files.npy"
+if not os.path.exists(uniques_file):
+    print "running uniques"
+    uniques= unique_words(files)
+    np.save(uniques_file,uniques)
+else:
+    uniques = np.load(uniques_file)
 window = 50
 weights = make_window_weights(window)
 wordmatrix= np.zeros((len(uniques)+1,len(uniques)+1))
-wordmatrix= textblobber(file6,uniques,wordmatrix,window,weights)
+
+for f in files[:3]:
+    print "textblobbing file "+str(f)+"\n"
+    wordmatrix= textblobber(f,uniques,wordmatrix,window,weights)
+    corrmatrix= np.corrcoef(wordmatrix)
